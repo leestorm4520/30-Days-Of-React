5501acae5d6f08c57b75f73b6cc1d00635edf756
DuyLe <minhduy4520@gmail.com>
2022-08-04 14:17:53 -0500

    update

--- 03_Day_Setting_Up/my-app/src/index.js
+++ 03_Day_Setting_Up/my-app/src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//index.js
+// importing the react and react-dom package
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// JSX element, header
+const header = (
+  <header>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Asabeneh Yetayeh</p>
+    <small>Oct 2, 2020</small>
+  </header>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JSX element, main
+const main = (
+  <main>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+
+// JSX element, footer
+const footer = (
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+// ReactDOM has the render method and the render method takes two argument
+ReactDOM.render(app, rootElement)
+// or
+//  ReactDOM.render([header, main, footer], rootElement)
