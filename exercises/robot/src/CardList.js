c43b955682c953b517ac2a034d91e317ddaf20c1
DuyLe <minhduy4520@gmail.com>
2022-10-26 17:42:28 -0500

    update

---
+++ exercises/robot/src/CardList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './Card'
+
+const CardList = ({robots}) =>{
+    const list = robots.map((robot)=>
+        <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email}/>
+    )
+    return (
+        <div>
+             {list}
+        </div>
+    )
+}
+
+export default CardList
